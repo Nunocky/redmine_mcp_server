1876e46452429e5c764a754e494fa556e974d07b
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2025-05-17 16:37:36 +0900

    環境変数

--- tools/News/get_news_tool.py
+++ tools/News/get_news_tool.py
@@ -33,7 +33,7 @@ def get_news(
     if redmine_url is None:
         redmine_url = os.environ.get("REDMINE_URL")
     if api_key is None:
-        api_key = os.environ.get("REDMINE_ADMIN_API_KEY")
+        api_key = os.environ.get("REDMINE_USER_API_KEY") or os.environ.get("REDMINE_ADMIN_API_KEY")
     if not redmine_url or not api_key:
         raise ValueError("redmine_url and api_key are required.")
 
