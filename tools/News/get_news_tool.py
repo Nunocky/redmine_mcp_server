a447e273787a641f22d75904e011bf1596441ba8
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2025-05-17 16:26:46 +0900

    News 仕様書の実装

--- tools/News/get_news_tool.py
+++ tools/News/get_news_tool.py
@@ -35,4 +35,8 @@ def get_news(
     }
 
 
-GetNewsTool = Tool.from_function(get_news, name="get_news", description="Get a list of news from Redmine.")
+GetNewsTool = Tool.from_function(
+    get_news,
+    name="get_news",
+    description="Get a list of news from Redmine.",
+)
